cedab5ce815b351158a790ad2db10d7fbedf21fb
Tyrell C <tyrellrcurry@gmail.com>
2023-07-04 22:01:11 -0700

    Active link added

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,34 +1,42 @@
-import styled, { css } from 'styled-components'
+import styled, { css } from "styled-components";
+import { NavLink } from "react-router-dom";
 
 const Navigation = () => {
+    const Nav = styled.nav`
+        display: flex;
+        background: grey;
+        width: 100%;
 
-  const Nav = styled.nav`
-    display: flex;
-    background: grey;
-    width: 100%;
+        ul li {
+            display: flex;
+            gap: 3rem;
+        }
 
-    ul li {
-      display: flex;
-      gap: 3rem;
-    }
+        ul li a {
+            color: #fff;
+            text-decoration: none;
+        }
 
-    ul li a {
-      color: #fff;
-      text-decoration: none;
-    }
-  `
+        ul li a:hover {
+            color: #00a8ff;
+        }
 
-  return (
-    <>
-    <Nav>
-      <ul>
-        <li>
-          <a href="/links">Links</a>
-          <a href="/profile">Profile</a>
-        </li>
-      </ul>
-    </Nav>
-    </>
-  )
-}
-export default Navigation
+        ul li a.active {
+            color: #00a8ff;
+        }
+    `;
+
+    return (
+        <>
+            <Nav>
+                <ul>
+                    <li>
+                        <NavLink to="/links">Links</NavLink>
+                        <NavLink to="/profile">Profile</NavLink>
+                    </li>
+                </ul>
+            </Nav>
+        </>
+    );
+};
+export default Navigation;
