4ca3df7149776226061fad0d6b6b89f0f8a7d5b9
Tyrell C <tyrellrcurry@gmail.com>
2023-07-04 22:01:15 -0700

    404 page

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router';
 import Home from './Pages/Home'
 import Links from './Pages/Links'
 import Profile from './Pages/Profile'
+import NotFound from './Pages/NotFound'
 import './App.css';
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/links" element={<Links />} />
         <Route path="/profile" element={<Profile />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </>
     );
