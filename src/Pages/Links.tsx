4ac73b5f07461c73659a36fa7c5ed778cd777bc1
Tyrell C <tyrellrcurry@gmail.com>
2023-07-09 22:00:39 -0700

    Links page content added

--- src/Pages/Links.tsx
+++ src/Pages/Links.tsx
@@ -1,10 +1,91 @@
 import Navigation from "../components/Navigation";
+import styled from "styled-components";
+import Iphone from "../images/illustration-phone-mockup.svg"
+import IphoneFinger from "../images/illustration-empty.svg"
 
 function Links() {
+    const LinksPage = styled.section`
+        display: flex;
+        justify-content: center;
+        padding: 3vh 10vw;
+
+        .left {
+            padding-top: 8vh;
+            padding-right: 10vw;
+        }
+        .right {
+            max-width: 500px;
+        }
+        .top h1 {
+            font-size: 2rem;
+        }
+        .top p {
+            color: #737373;
+        }
+        .top button {
+            width: 100%;
+            background-color: #fff;
+            border: 2px solid #633cff;
+            border-radius: 8px;
+            color: #633cff;
+            padding-top: 0.65rem;
+            padding-bottom: 0.65rem;
+            font-size: 1rem;
+            cursor: pointer;
+        }
+        .top button:hover {
+            background-color: #efebff;
+        }
+        button.save {
+            display: flex;
+            background-color: #beadff;
+            border: 2px solid #beadff;
+            color: #fff;
+            border-radius: 8px;
+            padding: 0.65rem 1rem;
+            font-size: 1rem;
+            font-weight: 600;
+            cursor: not-allowed;
+            margin-left: auto;
+        }
+        button.save:hover {
+            background-color: #beadff;
+        }
+        button.save.active {
+            background-color: #633cff;
+            border-color: #633cff;
+            cursor: pointer;
+        }
+    `;
     return (
         <>
             <Navigation />
-            <div>Links</div>
+            <LinksPage>
+                <div className="left">
+                    <img src={Iphone} alt="" />
+                </div>
+                <div className="right">
+                    <div className="top">
+                        <h1>Customize your links</h1>
+                        <p>
+                            Add/edit/remove links below and then share all your
+                            profiles with the world!
+                        </p>
+                        <button>+ Add new link</button>
+                    </div>
+                    <div className="bottom">
+                        <img src={IphoneFinger} alt="" />
+                        <h2>Let's get you started</h2>
+                        <p>
+                            Use the “Add new link” button to get started. Once
+                            you have more than one link, you can reorder and
+                            edit them. We’re here to help you share your
+                            profiles with everyone!
+                        </p>
+                    </div>
+                    <button className="save" disabled>Save</button>
+                </div>
+            </LinksPage>
         </>
     );
 }
