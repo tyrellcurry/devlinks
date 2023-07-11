cc2038101d8eb7552e56aab6da90b8708dfcdf71
Tyrell C <tyrellrcurry@gmail.com>
2023-07-10 21:56:14 -0700

    Beginning stages of the Profile page

--- src/Pages/Profile.tsx
+++ src/Pages/Profile.tsx
@@ -1,10 +1,13 @@
 import Navigation from "../components/Navigation";
+import SaveButton from "../components/SaveButton";
 
 function Profile() {
     return (
         <>
             <Navigation />
-            <div>Profile</div>
+            <h1>Profile Details</h1>
+            <p>Add your details to create a personal touch to your profile.</p>
+            <SaveButton />
         </>
     );
 }
