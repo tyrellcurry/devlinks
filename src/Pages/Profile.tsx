d9490a6782ed22f34a35b425c906a31a645ef5f6
Tyrell C <tyrellrcurry@gmail.com>
2023-07-12 22:09:16 -0700

    Styling and updates and added components

--- src/Pages/Profile.tsx
+++ src/Pages/Profile.tsx
@@ -1,13 +1,92 @@
+import styled from "styled-components";
 import Navigation from "../components/Navigation";
 import SaveButton from "../components/SaveButton";
+import IphoneLinks from "../components/IphoneLinks";
 
 function Profile() {
+    const Section = styled.section`
+        display: flex;
+        justify-content: center;
+        padding: 3vh 10vw;
+        .left {
+            padding-top: 8vh;
+            padding-right: 10vw;
+        }
+        .right {
+            width: 100%;
+            max-width: 600px;
+            display: flex;
+            flex-direction: column;
+            justify-content: space-between;
+        }
+    `;
+    const Form = styled.form`
+        display: flex;
+        flex-direction: column;
+
+        .row {
+            width: 100%;
+            display: flex;
+            justify-content: space-between;
+        }
+
+    `;
     return (
         <>
             <Navigation />
-            <h1>Profile Details</h1>
-            <p>Add your details to create a personal touch to your profile.</p>
-            <SaveButton />
+            <Section>
+                <div className="left">
+                    <IphoneLinks />
+                </div>
+                <div className="right">
+                    <div className="content">
+                        <h1>Profile Details</h1>
+                        <p>
+                            Add your details to create a personal touch to your
+                            profile.
+                        </p>
+                        <div className="pp">
+                            <p>Profile picture</p>
+                            <div className="upload">
+                                <div className="img">
+                                    <button>+ Upload Image</button>
+                                </div>
+                                <p>
+                                    Image must ve velow 1024x1024px. <br />
+                                    Use PNG or JPG format.
+                                </p>
+                            </div>
+                        </div>
+                        <Form>
+                            <div className="row">
+                                <label htmlFor="f_name">First name*</label>
+                                <input
+                                    type="text"
+                                    name="f_name"
+                                    placeholder="e.g. Luanna"
+                                />
+                            </div>
+                            <div className="row">
+                                <label htmlFor="l_name">Last name*</label>
+                                <input
+                                    type="text"
+                                    name="l_name"
+                                    placeholder="e.g. Lockhart"
+                                />
+                            </div>
+                            <div className="row">
+                                <label htmlFor="email">Email</label>
+                                <input
+                                    type="email"
+                                    name="email"
+                                    placeholder="e.g. luanna@email.com"
+                                />
+                            </div>
+                        </Form>
+                    </div>
+                    <SaveButton />
+                </div>
+            </Section>
         </>
     );
 }
