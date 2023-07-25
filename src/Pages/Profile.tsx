0cec47b3a92f280f7cbba9cc2ffadce096f46106
Tyrell C <tyrellrcurry@gmail.com>
2023-07-25 16:00:53 -0700

    Added image upload button

--- src/Pages/Profile.tsx
+++ src/Pages/Profile.tsx
@@ -1,10 +1,46 @@
 import styled from "styled-components";
-import PhotoIcon from "../images/icon-upload-image.svg"
+import React, { ChangeEvent, useState } from "react";
+import { ReactComponent as PhotoIcon } from "../images/icon-upload-image.svg";
 import Navigation from "../components/Navigation";
 import SaveButton from "../components/SaveButton";
 import IphoneLinks from "../components/IphoneLinks";
 
 function Profile() {
+    const [fileUploaded, setFileUploaded] = useState(false);
+    const [files, setFiles] = useState<FileList | null>(null); // Change the type if needed
+    const [uploadedImageUrl, setUploadedImageUrl] = useState<string | null>(null);
+  
+    const handleChange: React.ChangeEventHandler<HTMLInputElement> = (
+        event
+    ) => {
+        const fileList = event.target.files;
+        if (fileList) {
+            const file = fileList[0];
+
+            // Check if the file type is either PNG or JPG
+            if (file.type === "image/jpeg" || file.type === "image/png") {
+                const img = new Image();
+                img.onload = () => {
+                    // Check if the image is below 1024px x 1024px
+                    if (img.width <= 1024 && img.height <= 1024) {
+                        setFiles(fileList);
+                        setFileUploaded(true);
+                        setUploadedImageUrl(URL.createObjectURL(file)); // Use object URL for preview
+                        console.log("Uploaded image height:", img.height);
+                        console.log("Uploaded image width:", img.width);
+                    } else {
+                        console.log(
+                            "Image dimensions should be below 1024x1024px."
+                        );
+                    }
+                };
+                img.src = URL.createObjectURL(file);
+            } else {
+                console.log("Only PNG and JPG images are allowed.");
+            }
+        }
+    };
+      
     const Section = styled.section`
         display: flex;
         justify-content: center;
@@ -29,25 +65,33 @@ function Profile() {
             display: flex;
             align-items: center;
         }
-        .upload .img button {
+        .upload .img label {
             display: flex;
             flex-direction: column;
             align-items: center;
             padding: 3rem 1.5rem;
-            color: #633CFF;
+            color: ${uploadedImageUrl ? "#FFFFFF" : "#633cff"};
             font-weight: 600;
-            font-size: .85rem;
+            font-size: 0.85rem;
             cursor: pointer;
-            background-color: #EFEBFF;
+            background-color: #efebff;
+            background-image: ${uploadedImageUrl ? `url(${uploadedImageUrl})` : "none"};
+            background-position: center;
+            background-size: cover;
+            min-width: 125px;
             border: none;
             border-radius: 12px;
-            transition: background .125s ease-in-out;
+            transition: background 0.125s ease-in-out;
             &:hover {
                 background-color: #e7e1fe;
             }
         }
-        .upload .img button img {
-            padding-bottom: .5rem;
+        .upload .img input {
+            display: none;
+        }
+        label > svg path {
+            padding-bottom: 0.5rem;
+            fill: ${uploadedImageUrl ? "#FFFFFF" : "#633cff"};
         }
         .upload p {
             padding-left: 1rem;
@@ -62,7 +106,6 @@ function Profile() {
             display: flex;
             justify-content: space-between;
         }
-
     `;
     return (
         <>
@@ -82,7 +125,17 @@ function Profile() {
                             <p>Profile picture</p>
                             <div className="upload">
                                 <div className="img">
-                                    <button><img src={PhotoIcon} />+ Upload Image</button>
+                                    <label htmlFor="file">
+                                        <PhotoIcon />
+                                        {fileUploaded
+                                            ? "Change Image"
+                                            : "+ Upload Image"}
+                                    </label>
+                                    <input
+                                        type="file"
+                                        id="file"
+                                        onChange={handleChange}
+                                    />
                                 </div>
                                 <p>
                                     Image must be below 1024x1024px. <br />
